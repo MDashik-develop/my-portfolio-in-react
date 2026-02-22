77027999cab5dd5b973f3673d3cd27469471b07d
Md. Ashikur Rahman <MDashik.design.laptop@gmail.com>
2026-02-23 00:18:28 +0600

    bg imgs 3d objects link change to old

--- src/App.jsx
+++ src/App.jsx
@@ -12,16 +12,16 @@ function App() {
     <>
       <div className="background-shapes">
         <div className="shape-1 common-shape ">
-          <img src="https://bentofolio-react.vercel.app/assets/img/bg/banner-shape-1.png" alt="banner-shape-1" />
+          <img src="https://marveltheme.com/tf/html/bentofolio-html/assets/img/bg/banner-shape-1.png" alt="banner-shape-1" />
         </div>
         <div className="shape-2 common-shape">
-          <img src="https://bentofolio-react.vercel.app/assets/img/bg/banner-shape-1.png" alt="banner-shape-1" />
+          <img src="https://marveltheme.com/tf/html/bentofolio-html/assets/img/bg/banner-shape-1.png" alt="banner-shape-1" />
         </div>
         <div className="threed-shape-1 move-with-cursor" data-value="1">
-          <img src="https://bentofolio-react.vercel.app/assets/img/bg/object-3d-1.png" alt="object-3d-1" />
+          <img src="https://marveltheme.com/tf/html/bentofolio-html/assets/img/bg/object-3d-1.png" alt="object-3d-1" />
         </div>
         <div className="threed-shape-2 move-with-cursor" data-value="1">
-          <img src="https://bentofolio-react.vercel.app/assets/img/bg/object-3d-2.png" alt="object-3d-2" />
+          <img src="https://marveltheme.com/tf/html/bentofolio-html/assets/img/bg/object-3d-1.png" alt="object-3d-2" />
         </div>
       </div>
       <main className='w-full p-4'>
