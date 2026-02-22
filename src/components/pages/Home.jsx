5f660512b7d244050880f923322cbb3aa3fd1438
Md. Ashikur Rahman <MDashik.design.laptop@gmail.com>
2026-02-23 01:18:06 +0600

    icons set

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -35,7 +35,7 @@ function Home() {
                      <button className='w-1/2 bg-green-400 text-white p-4 text-xl font-semibold rounded-md'>Let's Talk</button>
                      <button className='w-1/2 border border-green-100 text-green-400 p-4 text-xl font-semibold rounded-md'>Let's Talk</button>
                   </div>
-                  <div className="flex items-center gap-2">
+                  <div className="flex items-center justify-between gap-2">
                      <a href="https://web.facebook.com/?_rdc=1&_rdr#" className="text-gray-700 hover:text-green-500 border border-gray-300 rounded-md p-3 duration-300">
                         <FaFacebook size={24} />
                      </a>
@@ -46,13 +46,13 @@ function Home() {
                         <FaTwitter size={24} />
                      </a>
                      <a href="mailto:mdashik.official.mail@gmail.com"
-                        className="w-1/2 bg-green-400 text-white p-4 text-xl font-semibold rounded-md text-center">
+                        className="ext-gray-700 hover:text-green-500 border border-gray-300 rounded-md p-3 duration-300">
                         <FaGoogle size={24} />
                      </a>
                      <a href="https://wa.me/8801795864799"
                         target="_blank"
                         rel="noopener noreferrer"
-                        className="w-1/2 border border-green-100 text-green-400 p-4 text-xl font-semibold rounded-md text-center">
+                        className="ext-gray-700 hover:text-green-500 border border-gray-300 rounded-md p-3 duration-300">
                         <FaWhatsapp size={24} />
                      </a>
                   </div>
